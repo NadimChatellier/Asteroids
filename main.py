15c79c02c92ab226bd1555c364b8167a80af8ea5
Mithrildagger <nadimchatellier@gmail.com>
2024-08-22 16:35:21 +0100

    Commit to display P1 ship

--- main.py
+++ main.py
@@ -3,17 +3,24 @@
 # throughout this file
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    P1 = Player(x, y)
+    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        P1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
         
